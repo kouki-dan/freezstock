172d86ca66a26cef3b2bda2621ab8b7f5eebed21
Kouki Saito <dan.addr.skd@gmail.com>
2023-03-23 23:40:16 +0900

    Initialize firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
